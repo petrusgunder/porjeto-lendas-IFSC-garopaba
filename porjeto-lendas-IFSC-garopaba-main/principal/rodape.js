521c36396f8d0181c968a3842629e3b6087c8408
Lucas02042010 <lucas.fm@aluno.ifsc.edu.br>
2026-06-17 17:05:35 -0300

    super correção

---
+++ porjeto-lendas-IFSC-garopaba-main/principal/rodape.js
@@ -0,0 +1,5 @@
+document.getElementById('footer').innerHTML = `
+    <footer class="simple-footer">
+        <p>&copy; <span id="current-year"></span> Nossos Contos Nossas Lendas I IFSC Garopaba.</p>
+    </footer>
+`;
